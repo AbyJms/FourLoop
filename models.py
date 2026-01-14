45205e2021a2800fecbb092e917497414456d330
Arshiya <arshiyasulfikkar@gmail.com>
2026-01-15 03:31:10 +0530

    harithakarma

--- models.py
+++ models.py
@@ -23,3 +23,42 @@ class User(db.Model, UserMixin):
     def check_password(self, password: str) -> bool:
         from werkzeug.security import check_password_hash
         return check_password_hash(self.password_hash, password)
+
+
+from datetime import datetime
+
+class WasteRequest(db.Model):
+    __tablename__ = "waste_requests"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    home_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    waste_type = db.Column(db.String(20), nullable=False)
+    # food | plastic | mixed
+
+    status = db.Column(db.String(20), default="pending")
+    # pending | accepted | completed | flagged
+
+    assigned_harithakarmasena_id = db.Column(
+        db.Integer, db.ForeignKey("users.id"), nullable=True
+    )
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    class WasteViolation(db.Model):
+    __tablename__ = "waste_violations"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    home_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    request_id = db.Column(db.Integer, db.ForeignKey("waste_requests.id"))
+
+    reason = db.Column(db.String(255), nullable=False)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+
+
+
+
