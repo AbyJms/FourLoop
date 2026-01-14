f37df075b472c6020ce9cd525bb4e5cea936508d
Arshiya <arshiyasulfikkar@gmail.com>
2026-01-15 03:50:01 +0530

    changes2

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ def create_app():
     login_manager.init_app(app)
 
     @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
+             def load_user(user_id):
+              return User.query.get(int(user_id))
 
     # ---------------- API ROUTES ----------------
 
